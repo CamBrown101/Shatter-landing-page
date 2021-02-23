e9ccbf8279ce40099513d180589d5060a4ec19e4
cambrown101 <cameron-brown@live.com>
2021-02-22 16:52:05 -0800

    Block out elements

---
+++ src/Components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div>
+        <h1>LOGO</h1>
+      </div>
+      <h1>Shatter</h1>
+      <h3>THERE IS NO VICTORY WITHOUT A BATTLE.</h3>
+      <form>
+        <input></input>
+        <button type="submit">LEVEL UP</button>
+      </form>
+    </div>
+  );
+};
+
+export default Main;
