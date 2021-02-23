a5b8e8c853330b7cbbdd7a831ec8e714182db3eb
cambrown101 <cameron-brown@live.com>
2021-02-22 17:25:20 -0800

    Start replacing placeholders

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Main.scss';
+import logo from '../images/group1_fixed.png';
 
 const Main = () => {
   return (
     <div className="main-container">
       <div>
-        <h1>LOGO</h1>
+        <img className="logo" src={logo}></img>
       </div>
-      <h1>Shatter</h1>
       <h3>THERE IS NO VICTORY WITHOUT A BATTLE.</h3>
-      <form>
-        <input></input>
+      <form className="form-container">
+        <input placeholder="Enter Email Address"></input>
         <button type="submit">LEVEL UP</button>
       </form>
     </div>
