abb76f32c40f1bbe680266c686bb116097321603
cambrown101 <cameron-brown@live.com>
2021-02-24 20:21:37 -0800

    Add Mailchimp Component

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Main.scss';
 import logo from '../images/group1_fixed.png';
+import { MailchimpForm } from './MailchimpForm';
 
 const Main = () => {
   const handleSubmit = (event) => {
@@ -13,10 +14,11 @@ const Main = () => {
         <img alt="Shatter Logo" className="logo" src={logo}></img>
       </div>
       <h3 className="quote">THERE IS NO VICTORY WITHOUT A BATTLE.</h3>
-      <form className="form-container" onSubmit={handleSubmit}>
+      {/* <form className="form-container" onSubmit={handleSubmit}>
         <input type="email" placeholder="Enter Email Address"></input>
         <button type="submit">LEVEL UP</button>
-      </form>
+      </form> */}
+      <MailchimpForm />
     </div>
   );
 };
